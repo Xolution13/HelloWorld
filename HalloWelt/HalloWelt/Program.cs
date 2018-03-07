a6a4ae23f464d6ec91be4cde49de4227205d2781
Xolution13 <e-lewandowski@web.de>
2018-03-07 17:46:33 +0100

    Variables

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -6,7 +6,11 @@ namespace HalloWelt
     {
         static void Main()
         {
+            // Declaration and Initialization
+            String username = "Xolution13";
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Username: " + username);
             Console.ReadKey();
         }
     }
